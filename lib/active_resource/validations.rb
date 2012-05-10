4977f48f0cc92caf9e341e62da515529f5474120
Matt Smith <matt@nearapogee.com>
2012-05-09 19:25:09 -0700

    fix remote error parsing.

--- lib/active_resource/validations.rb
+++ lib/active_resource/validations.rb
@@ -132,11 +132,11 @@ module ActiveResource
     # Loads the set of remote errors into the object's Errors based on the
     # content-type of the error-block received.
     def load_remote_errors(remote_errors, save_cache = false ) #:nodoc:
-      case self.class.format
-      when ActiveResource::Middleware::Formats[:xml]
-        errors.from_xml(remote_errors.response[:body], save_cache)
-      when ActiveResource::Middleware::Formats[:json]
-        errors.from_json(remote_errors.response[:body], save_cache)
+      case
+      when self.class.format == ActiveResource::Middleware::Formats[:xml]
+        errors.from_xml(remote_errors.response.body, save_cache)
+      when self.class.format == ActiveResource::Middleware::Formats[:json]
+        errors.from_json(remote_errors.response.body, save_cache)
       end
     end
 
